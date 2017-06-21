9f8fb5817914e90555d186f316b02207be3e8cf2
Michael Spencer <michael4@hawaii.edu>
2017-06-20 14:49:12 -1000

    set to 2 col layout

--- client/categoryDropdown.jsx
+++ client/categoryDropdown.jsx
@@ -1,24 +1,35 @@
+import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import React, { Component } from 'react';
+import './main.html';
 
 
-
-export default class categoryDropdown extends Component {
-    render() {
+class CategoryDropdown extends Component{
+    handleClick() {
+        for (var speech = '', i = 0; i < 10000; i++) {
+            speech += 'blah ';
+        }
+        alert(speech);
     }
-}
 
-/*
+    render() {
+        return
+        <div className="ui selection dropdown">
+            <input type="hidden" name="gender" />
+                <i className="dropdown icon"></i>
+                <div className="default text">Gender</div>
+                <div className="menu">
+                    <div className="item" data-value="1">Male</div>
+                    <div className="item" data-value="0">Female</div>
+                </div>
+        </div>;
+    }
+};
 
-<div class="ui selection dropdown">
-    <input type="hidden" name="gender">
-        <i class="dropdown icon"></i>
-        <div class="default text">Gender</div>
-        <div class="menu">
-            <div class="item" data-value="1">Male</div>
-            <div class="item" data-value="0">Female</div>
-        </div>
-</div>
 
-    */
+
+
+
+Meteor.startup(() => {
+    //render(<CategoryDropdown />, document.getElementById('categoryDropdownDiv'));
+});
